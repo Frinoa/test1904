f4756e4b277bdd0187110fa35f35ea739a25867c
Frinoa <375924333@qq.com>
2022-02-07 13:00:20 +0800

    123

--- src/com/bjpowernode/domain/Person.java
+++ src/com/bjpowernode/domain/Person.java
@@ -1,5 +1,7 @@
 package com.bjpowernode.domain;
 
+import com.sun.xml.internal.org.jvnet.fastinfoset.sax.FastInfosetReader;
+
 /**
  * @author nan
  * @create 2022-02-06-22:02
@@ -7,5 +9,6 @@ package com.bjpowernode.domain;
 public class Person {
     private String id;
     private String name;
+    private String phone;
 
 }
